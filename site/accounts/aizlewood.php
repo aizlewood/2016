6ad8f9841d8ce5f20f9aaa013716d4f5c5972d66
Aizlewood <jon@clearleft.com>
2015-09-10 21:29:07 +0100

    Fresh install of Kirby

---
+++ site/accounts/aizlewood.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: aizlewood
+email: j.aizlewood@gmail.com
+password: >
+  $2a$10$IUz5ek.b58ARnpHZfKkH4eYf/5W/6FEUo50IlqQZNquvKSuQkcGPu
+language: en
+role: admin
